bfae5fa463cf721e304eac5e831d755497cdc6f3
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-11-27 22:47:18 -0600

    thymeleaf conditional switch-case exercise

--- 02. model-view-controller/workbook 2.2/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ 02. model-view-controller/workbook 2.2/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -1,14 +1,35 @@
 package com.ltp.workbook;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 public class WorkbookController {
-    
-    @GetMapping("/")
-    public String getShows() {
-        return "shows";
-    }
+    //creating object of Show class.
+//    Show show1 = new Show("Breaking Bad", "Ozymandias", 10.0);
+//    Show show2 = new Show("Attack on Titan", "Hero", 9.9);
+//    Show show3 = new Show("Attack on Titan", "Perfect Game", 9.9);
+//    Show show4 = new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9);
+//    Show show5 = new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9);
+
+//    @GetMapping("/shows")
+    //
+//    public String getShows(Model model) {
+//        model.addAttribute("show1",show1);
+//        model.addAttribute("show2",show2);
+//        model.addAttribute("show3",show3);
+//        model.addAttribute("show4",show4);
+//        model.addAttribute("show5",show5);
+//        return "shows";
+//    }
+
+
+
+
 
 }
